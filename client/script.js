087928ac0743c6fed6a81d2eeac95b8b9333cda9
Syed Anas Bukhari <anas14529@gmail.com>
2024-03-01 23:55:51 +0500

    New commit.

--- client/script.js
+++ client/script.js
@@ -6,6 +6,13 @@ const hello = () => {
     console.log('Hello, world!');
 };
 
+const openDB = indexedDB.open('text_editor', 1);
+openDB.onupgradeneeded = function() {
+    const openDB = indexedDB.open('text_editor', 1);
+    const db = openDB.result;
+    const store = db.createObjectStore('entries', { keyPath: 'id' });
+  };
+
 hello();
 // Function to show data from IndexedDB
 function showData() {
@@ -17,6 +24,7 @@ function showData() {
 
         const getRequest = store.get(1); // Get content from the database
 
+        console.log('1');
         getRequest.onsuccess = function () {
             const data = getRequest.result;
             if (data && data.content) {
@@ -32,6 +40,7 @@ function showData() {
                     lineDiv.innerHTML = `<span class="line-no">${i}</span><span class="line-content">${line}</span>`;
                     outputDiv.appendChild(lineDiv);
                     i++;
+                    console.log('AA');
                 });
                 currentLine.textContent = i;
             }
@@ -49,10 +58,16 @@ function saveContent(content) {
 
         const getRequest = store.get(1); // Get existing content from the database
 
+        outputDiv.innerHTML = ''; // Clear previous content
+        textarea.value = ''; // Clear the textarea after saving the content
+
         if (content === 'clear') {
-            store.clear(); // Clear the object store
-            outputDiv.innerHTML = ''; // Clear previous content
-            currentLine.textContent = 1;
+            store.put({ id: 1, content: "" });
+            tx.oncomplete = function () {
+                console.log('Cleared from IndexedDB');
+                showData(); // Call showData to display the saved content in the output div
+                currentLine.textContent = 1;
+            };
         } else {
             getRequest.onsuccess = function () {
                 const data = getRequest.result;
@@ -65,14 +80,13 @@ function saveContent(content) {
 
                 // Put the merged content into the object store
                 store.put({ id: 1, content: mergedContent });
+                tx.oncomplete = function () {
+                    showData(); // Call showData to display the saved content in the output div
+                    console.log('Content saved to IndexedDB');
+                };
             };
         }
         
-        tx.oncomplete = function () {
-            console.log('Contents saved to IndexedDB');
-            textarea.value = ''; // Clear the textarea after saving the content
-            showData(); // Call showData to display the saved content in the output div
-        };
     };
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,44 +5,48 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './client/script.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
+    entry: './client/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
         ]
-      }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './client/index.html'
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: './client/styles.css', to: 'styles.css' } // Adjust source and destination paths accordingly
+            ]
+        }), new CopyPlugin({
+            patterns: [
+                { from: './client/img', to: 'img' }
+            ]
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './client/index.html'
-    }),
-    new WorkboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css'
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: './client/styles.css', to: 'styles.css' } // Adjust source and destination paths accordingly
-      ]
-    })
-  ]
 };
